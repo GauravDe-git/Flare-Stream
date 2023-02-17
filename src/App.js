4feb1b4967b94179764dadef066d2afaf0cd22d5
GauravDe-git <gm.gauravde@gmail.com>
2023-02-17 18:06:51 +0530

    Feat: Tailwind Setup

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import React from "react";
-import ReactDOM from "react-dom/client"
+import ReactDOM from "react-dom/client";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 const App = () => {
-    return(
-        <>
-        <h1>Hello</h1>
-         <h2>test</h2>
-         
-        </>
-    )
-}
+  return (
+    <>
+      <h1 className="p-2 bg-green-300">Hello</h1>
+    </>
+  );
+};
 
-root.render(<App/>);
+root.render(<App />);
