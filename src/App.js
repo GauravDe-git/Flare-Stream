8d5b802fd8377b62c5f0a0e33b9b644b1f642941
GauravDe-git <gm.gauravde@gmail.com>
2023-02-17 17:53:16 +0530

    Feat: Initial Project Setup

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom/client"
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const App = () => {
+    return(
+        <>
+        <h1>Hello</h1>
+         <h2>test</h2>
+         
+        </>
+    )
+}
+
+root.render(<App/>);
